b1156eb44575818f17d40a76daef2d5a735fdb25
Michelle Hood <meesh121@gmail.com>
2015-07-15 20:46:53 -0400

    tests pass and I'm going for html

---
+++ app/controllers/palettes_controller.rb
@@ -0,0 +1,59 @@
+class PalettesController < ApplicationController
+
+  def index
+    @palettes = Palette.all
+    respond_to do |format|
+      format.json { render json: @palettes }
+    end
+  end
+
+  def show
+    @palette = get_palette
+    respond_to do |format|
+      format.json { render json: @palette }
+    end
+  end
+
+  def create
+    @palette = Palette.new(palette_params)
+    respond_to do |format|
+      if @palette.save
+        format.json { render json: @palette, status: :created }
+      else
+        format.json { render json: @palette.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @palette = get_palette
+    respond_to do |format|
+      if @palette.update_attributes(palette_params)
+        format.json { render json: @palette }
+      else
+        format.json { render json: @palette.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @palette = get_palette
+    respond_to do |format|
+      if @palette.destroy
+        format.json { redirect_to palettes_path, status: :destroyed }
+      else
+        format.json { render json: @palette.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def get_palette
+    Palette.find(params[:id])
+  end
+
+  def palette_params
+    params.require(:palette).permit(:name, :dom_one, :dom_two, :sec_one, :sec_two, :pop)
+  end
+end
